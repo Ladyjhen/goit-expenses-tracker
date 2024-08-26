2bb8a7686af811a5f975c5928b6926a948d05927
Ladyjhen <145974043+Ladyjhen@users.noreply.github.com>
2024-08-26 15:19:46 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { GlobalStyles } from 'styles/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename="/project_blackout">
+  <BrowserRouter basename="/goit-expenses-tracker">
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
